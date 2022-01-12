af3e504a0585fc3af0b2cb8d650baaa82c01f941
Burri Akhil Reddy <akhil15935@gmail.com>
2022-01-12 16:58:34 +0530

    Created pages for the sub-menus

---
+++ options.py
@@ -0,0 +1,23 @@
+#!/home/akhil/anaconda3/bin/python
+import variables 
+from form import form
+from env import cgiRoute
+def nav(inp):
+    # function to return the title of the page
+    print(f"""
+        <nav style="position: relative;">
+        <a href="{cgiRoute}" style="position: absolute; left: 10px;">
+        <i class="bi bi-house"></i>
+        </a>
+        <a href="/cgi-bin/{inp}">{inp.capitalize()}</a>
+        </nav>
+    """.format(inp.upper()))
+def output(platform):
+    # based on the platform the selected it will go to a route where sub menu options will be displayed
+    menu_main=variables.values()[1]
+    nav(platform.upper())
+    print("<div class='main_menu'>")
+    for i in menu_main[platform.lower()]:
+        if(menu_main[platform.lower()][i]!=None):
+            print("<h1 class='menu'><a href='{2}menu/{0}'>{1}</a></h1>".format(menu_main[platform.lower()][i],i.capitalize(),platform))
+    print("</div>")
